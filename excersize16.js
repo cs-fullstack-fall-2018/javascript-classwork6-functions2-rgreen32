f0ef64f8b2b1dc843b3697057cfad732a649dc6d
Rey <ray.greenm@gmail.com>
2018-07-31 13:19:58 -0500

    Rey

---
+++ excersize16.js
@@ -0,0 +1,13 @@
+var num = parseInt(prompt("Gimme your favorite number"));
+
+function abboveOrBelow(var number){
+    if(number > 100){
+        alert("your number is abover")
+    }if else(number < 100){
+        alert("your number is belower")
+    }else{
+        alert("its 100")
+    }
+}
+
+abboveOrBelow(num);
